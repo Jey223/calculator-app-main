30a4284ff1f994b637b848e14bcd06d62a6057cd
Jey223 <julieudoh7@gmail.com>
2022-11-26 10:08:35 +0100

    added toogle function

---
+++ script.js
@@ -0,0 +1,252 @@
+let toggles = document.getElementsByTagName('input');
+let arr = [...toggles];
+
+
+for (let i = 0; i < arr.length; i++) {
+    arr[i].addEventListener('click', () => {
+        let newarr = arr[i].style.opacity = "1";
+        if (newarr) {
+            arr[i].style.cursor = "pointer";
+        }
+
+
+        let toggle1 = document.getElementById("toggleOne");
+        let toggle2 = document.getElementById("toggleTwo");
+
+        if (arr[i] === toggle1) {
+            document.getElementsByTagName("main")[0].style.backgroundColor = "hsl(222, 26%, 31%)";
+            document.getElementById("toggle-bg").style.backgroundColor = "hsl(223, 31%, 20%)";
+            document.getElementsByTagName("h1")[0].style.color = "hsl(0, 0%, 100%)";
+            document.getElementsByTagName("h2")[0].style.color = "hsl(0, 0%, 100%)";
+            document.getElementById("p").style.color = "hsl(0, 0%, 100%)";
+
+            document.querySelector("#screen").style.backgroundColor = "hsl(224, 36%, 15%)";
+            document.querySelector("#screen-text").style.color = "hsl(0, 0%, 100%)";
+
+            document.getElementById("buttons").style.backgroundColor = "hsl(223, 31%, 20%)";
+
+
+
+
+            document.getElementsByTagName("button")[0].style.backgroundColor = "hsl(30, 25%, 89%)";
+            document.getElementsByTagName("button")[0].style.boxShadow = "0px 3.5px 1px 0px hsl(28, 16%, 65%)";
+
+            document.getElementsByTagName("button")[1].style.backgroundColor = "hsl(30, 25%, 89%)";
+            document.getElementsByTagName("button")[1].style.boxShadow = "0px 3.5px 1px 0px hsl(28, 16%, 65%)";
+
+            document.getElementsByTagName("button")[2].style.backgroundColor = "hsl(30, 25%, 89%)";
+            document.getElementsByTagName("button")[2].style.boxShadow = "0px 3.5px 1px 0px hsl(28, 16%, 65%)";
+
+            document.getElementsByTagName("button")[3].style.backgroundColor = "hsl(225, 21%, 49%)";
+            document.getElementsByTagName("button")[3].style.boxShadow = "0px 3.5px 1px 0px hsl(224, 28%, 35%)";
+
+            document.getElementsByTagName("button")[4].style.backgroundColor = "hsl(30, 25%, 89%)";
+            document.getElementsByTagName("button")[4].style.boxShadow = "0px 3.5px 1px 0px hsl(28, 16%, 65%)";
+
+            document.getElementsByTagName("button")[5].style.backgroundColor = "hsl(30, 25%, 89%)";
+            document.getElementsByTagName("button")[5].style.boxShadow = "0px 3.5px 1px 0px hsl(28, 16%, 65%)";
+
+            document.getElementsByTagName("button")[6].style.backgroundColor = "hsl(30, 25%, 89%)";
+            document.getElementsByTagName("button")[6].style.boxShadow = "0px 3.5px 1px 0px hsl(28, 16%, 65%)";
+
+            document.getElementsByTagName("button")[7].style.backgroundColor = "hsl(30, 25%, 89%)";
+            document.getElementsByTagName("button")[7].style.boxShadow = "0px 3.5px 1px 0px hsl(28, 16%, 65%)";
+
+            document.getElementsByTagName("button")[8].style.backgroundColor = "hsl(30, 25%, 89%)";
+            document.getElementsByTagName("button")[8].style.boxShadow = "0px 3.5px 1px 0px hsl(28, 16%, 65%)";
+
+            document.getElementsByTagName("button")[9].style.backgroundColor = "hsl(30, 25%, 89%)";
+            document.getElementsByTagName("button")[9].style.boxShadow = "0px 3.5px 1px 0px hsl(28, 16%, 65%)";
+
+            document.getElementsByTagName("button")[10].style.backgroundColor = "hsl(30, 25%, 89%)";
+            document.getElementsByTagName("button")[10].style.boxShadow = "0px 3.5px 1px 0px hsl(28, 16%, 65%)";
+
+            document.getElementsByTagName("button")[11].style.backgroundColor = "hsl(30, 25%, 89%)";
+            document.getElementsByTagName("button")[11].style.boxShadow = "0px 3.5px 1px 0px hsl(28, 16%, 65%)";
+
+            document.getElementsByTagName("button")[12].style.backgroundColor = "hsl(30, 25%, 89%)";
+            document.getElementsByTagName("button")[12].style.boxShadow = "0px 3.5px 1px 0px hsl(28, 16%, 65%)";
+
+            document.getElementsByTagName("button")[13].style.backgroundColor = "hsl(30, 25%, 89%)";
+            document.getElementsByTagName("button")[13].style.boxShadow = "0px 3.5px 1px 0px hsl(28, 16%, 65%)";
+
+            document.getElementsByTagName("button")[14].style.backgroundColor = "hsl(30, 25%, 89%)";
+            document.getElementsByTagName("button")[14].style.boxShadow = "0px 3.5px 1px 0px hsl(28, 16%, 65%)";
+
+            document.getElementsByTagName("button")[15].style.backgroundColor = "hsl(30, 25%, 89%)";
+            document.getElementsByTagName("button")[15].style.boxShadow = "0px 3.5px 1px 0px hsl(28, 16%, 65%)";
+
+            document.getElementsByTagName("button")[16].style.backgroundColor = "hsl(225, 21%, 49%)";
+            document.getElementsByTagName("button")[16].style.boxShadow = "0px 3.5px 1px 0px hsl(224, 28%, 35%)";
+            
+            document.getElementsByTagName("button")[17].style.backgroundColor = "hsl(6, 63%, 50%)";
+            document.getElementsByTagName("button")[17].style.boxShadow = "0px 3.5px 1px 0px hsl(6, 70%, 34%)";
+
+
+
+
+
+        }
+        else if (arr[i] === toggle2) {
+            document.getElementsByTagName("main")[0].style.backgroundColor = "hsl(0, 0%, 90%)";
+            document.getElementById("toggle-bg").style.backgroundColor = "hsl(0, 5%, 81%)";
+            document.getElementsByTagName("h1")[0].style.color = "hsl(60, 10%, 19%)";
+            document.getElementsByTagName("h2")[0].style.color = "hsl(60, 10%, 19%)";
+            document.getElementById("p").style.color = "hsl(60, 10%, 19%)";
+
+
+            document.querySelector("#screen").style.backgroundColor = "hsl(0, 0%, 93%)";
+            document.querySelector("#screen-text").style.color = "hsl(60, 10%, 19%)";
+
+            document.getElementById("buttons").style.backgroundColor = "hsl(0, 5%, 81%)";
+
+
+            document.getElementsByTagName("button")[0].style.backgroundColor = "hsl(45, 7%, 89%)";
+            document.getElementsByTagName("button")[0].style.boxShadow = "0px 3.5px 1px 0px hsl(35, 11%, 61%)";
+
+            document.getElementsByTagName("button")[1].style.backgroundColor = "hsl(45, 7%, 89%)";
+            document.getElementsByTagName("button")[1].style.boxShadow = "0px 3.5px 1px 0px hsl(35, 11%, 61%)";
+
+            document.getElementsByTagName("button")[2].style.backgroundColor = "hsl(45, 7%, 89%)";
+            document.getElementsByTagName("button")[2].style.boxShadow = "0px 3.5px 1px 0px hsl(35, 11%, 61%)";
+
+            document.getElementsByTagName("button")[3].style.backgroundColor = "hsl(185, 42%, 37%)";
+            document.getElementsByTagName("button")[3].style.boxShadow = "0px 3.5px 1px 0px hsl(185, 58%, 25%)";
+
+            document.getElementsByTagName("button")[4].style.backgroundColor = "hsl(45, 7%, 89%)";
+            document.getElementsByTagName("button")[4].style.boxShadow = "0px 3.5px 1px 0px hsl(35, 11%, 61%)";
+
+            document.getElementsByTagName("button")[5].style.backgroundColor = "hsl(45, 7%, 89%)";
+            document.getElementsByTagName("button")[5].style.boxShadow = "0px 3.5px 1px 0px hsl(35, 11%, 61%)";
+
+            document.getElementsByTagName("button")[6].style.backgroundColor = "hsl(45, 7%, 89%)";
+            document.getElementsByTagName("button")[6].style.boxShadow = "0px 3.5px 1px 0px hsl(35, 11%, 61%)";
+
+            document.getElementsByTagName("button")[7].style.backgroundColor = "hsl(45, 7%, 89%)";
+            document.getElementsByTagName("button")[7].style.boxShadow = "0px 3.5px 1px 0px hsl(35, 11%, 61%)";
+
+            document.getElementsByTagName("button")[8].style.backgroundColor = "hsl(45, 7%, 89%)";
+            document.getElementsByTagName("button")[8].style.boxShadow = "0px 3.5px 1px 0px hsl(35, 11%, 61%)";
+
+            document.getElementsByTagName("button")[9].style.backgroundColor = "hsl(45, 7%, 89%)";
+            document.getElementsByTagName("button")[9].style.boxShadow = "0px 3.5px 1px 0px hsl(35, 11%, 61%)";
+
+            document.getElementsByTagName("button")[10].style.backgroundColor = "hsl(45, 7%, 89%)";
+            document.getElementsByTagName("button")[10].style.boxShadow = "0px 3.5px 1px 0px hsl(35, 11%, 61%)";
+
+            document.getElementsByTagName("button")[11].style.backgroundColor = "hsl(45, 7%, 89%)";
+            document.getElementsByTagName("button")[11].style.boxShadow = "0px 3.5px 1px 0px hsl(35, 11%, 61%)";
+
+            document.getElementsByTagName("button")[12].style.backgroundColor = "hsl(45, 7%, 89%)";
+            document.getElementsByTagName("button")[12].style.boxShadow = "0px 3.5px 1px 0px hsl(35, 11%, 61%)";
+
+            document.getElementsByTagName("button")[13].style.backgroundColor = "hsl(45, 7%, 89%)";
+            document.getElementsByTagName("button")[13].style.boxShadow = "0px 3.5px 1px 0px hsl(35, 11%, 61%)";
+
+            document.getElementsByTagName("button")[14].style.backgroundColor = "hsl(45, 7%, 89%)";
+            document.getElementsByTagName("button")[14].style.boxShadow = "0px 3.5px 1px 0px hsl(35, 11%, 61%)";
+
+            document.getElementsByTagName("button")[15].style.backgroundColor = "hsl(45, 7%, 89%)";
+            document.getElementsByTagName("button")[15].style.boxShadow = "0px 3.5px 1px 0px hsl(35, 11%, 61%)";
+
+            document.getElementsByTagName("button")[16].style.backgroundColor = "hsl(185, 42%, 37%)";
+            document.getElementsByTagName("button")[16].style.boxShadow = "0px 3.5px 1px 0px hsl(185, 58%, 25%)";
+            
+            document.getElementsByTagName("button")[17].style.backgroundColor = "hsl(25, 98%, 40%)";
+            document.getElementsByTagName("button")[17].style.boxShadow = "0px 3.5px 1px 0px hsl(25, 99%, 27%)";
+
+
+
+        }
+        else {
+            document.getElementsByTagName("main")[0].style.backgroundColor = "hsl(268, 75%, 9%)";
+            document.getElementById("toggle-bg").style.backgroundColor = "hsl(268, 71%, 12%)";
+            document.getElementsByTagName("h1")[0].style.color = "hsl(52, 100%, 62%)";
+            document.getElementsByTagName("h2")[0].style.color = "hsl(52, 100%, 62%)";
+            document.getElementById("p").style.color = "hsl(52, 100%, 62%)";
+
+            document.querySelector("#screen").style.backgroundColor = "hsl(268, 71%, 12%)";
+            document.querySelector("#screen-text").style.color = "hsl(52, 100%, 62%)";
+
+
+            document.getElementById("buttons").style.backgroundColor = "hsl(268, 71%, 12%)";
+
+            document.getElementsByTagName("button")[0].style.backgroundColor = "hsl(268, 47%, 21%)";
+            document.getElementsByTagName("button")[0].style.boxShadow = "0px 3.5px 1px 0px hsl(290, 70%, 36%)";
+
+            document.getElementsByTagName("button")[1].style.backgroundColor = "hsl(268, 47%, 21%)";
+            document.getElementsByTagName("button")[1].style.boxShadow = "0px 3.5px 1px 0px hsl(290, 70%, 36%)";
+
+            document.getElementsByTagName("button")[2].style.backgroundColor = "hsl(268, 47%, 21%)";
+            document.getElementsByTagName("button")[2].style.boxShadow = "0px 3.5px 1px 0px hsl(290, 70%, 36%)";
+
+            document.getElementsByTagName("button")[3].style.backgroundColor = "hsl(281, 89%, 26%)";
+            document.getElementsByTagName("button")[3].style.boxShadow = "0px 3.5px 1px 0px hsl(285, 91%, 52%)";
+
+            document.getElementsByTagName("button")[4].style.backgroundColor = "hsl(268, 47%, 21%)";
+            document.getElementsByTagName("button")[4].style.boxShadow = "0px 3.5px 1px 0px hsl(290, 70%, 36%)";
+
+            document.getElementsByTagName("button")[5].style.backgroundColor = "hsl(268, 47%, 21%)";
+            document.getElementsByTagName("button")[5].style.boxShadow = "0px 3.5px 1px 0px hsl(290, 70%, 36%)";
+
+            document.getElementsByTagName("button")[6].style.backgroundColor = "hsl(268, 47%, 21%)";
+            document.getElementsByTagName("button")[6].style.boxShadow = "0px 3.5px 1px 0px hsl(290, 70%, 36%)";
+
+            document.getElementsByTagName("button")[7].style.backgroundColor = "hsl(268, 47%, 21%)";
+            document.getElementsByTagName("button")[7].style.boxShadow = "0px 3.5px 1px 0px hsl(290, 70%, 36%)";
+
+            document.getElementsByTagName("button")[8].style.backgroundColor = "hsl(268, 47%, 21%)";
+            document.getElementsByTagName("button")[8].style.boxShadow = "0px 3.5px 1px 0px hsl(290, 70%, 36%)";
+
+            document.getElementsByTagName("button")[9].style.backgroundColor = "hsl(268, 47%, 21%)";
+            document.getElementsByTagName("button")[9].style.boxShadow = "0px 3.5px 1px 0px hsl(290, 70%, 36%)";
+
+            document.getElementsByTagName("button")[10].style.backgroundColor = "hsl(268, 47%, 21%)";
+            document.getElementsByTagName("button")[10].style.boxShadow = "0px 3.5px 1px 0px hsl(290, 70%, 36%)";
+
+            document.getElementsByTagName("button")[11].style.backgroundColor = "hsl(268, 47%, 21%)";
+            document.getElementsByTagName("button")[11].style.boxShadow = "0px 3.5px 1px 0px hsl(290, 70%, 36%)";
+
+            document.getElementsByTagName("button")[12].style.backgroundColor = "hsl(268, 47%, 21%)";
+            document.getElementsByTagName("button")[12].style.boxShadow = "0px 3.5px 1px 0px hsl(290, 70%, 36%)";
+
+            document.getElementsByTagName("button")[13].style.backgroundColor = "hsl(268, 47%, 21%)";
+            document.getElementsByTagName("button")[13].style.boxShadow = "0px 3.5px 1px 0px hsl(290, 70%, 36%)";
+
+            document.getElementsByTagName("button")[14].style.backgroundColor = "hsl(268, 47%, 21%)";
+            document.getElementsByTagName("button")[14].style.boxShadow = "0px 3.5px 1px 0px hsl(290, 70%, 36%)";
+
+            document.getElementsByTagName("button")[15].style.backgroundColor = "hsl(268, 47%, 21%)";
+            document.getElementsByTagName("button")[15].style.boxShadow = "0px 3.5px 1px 0px hsl(290, 70%, 36%)";
+
+            document.getElementsByTagName("button")[16].style.backgroundColor = "hsl(281, 89%, 26%)";
+            document.getElementsByTagName("button")[16].style.boxShadow = "0px 3.5px 1px 0px hsl(285, 91%, 52%)";
+
+            document.getElementsByTagName("button")[17].style.backgroundColor = "hsl(176, 100%, 44%)";
+            document.getElementsByTagName("button")[17].style.boxShadow = "0px 3.5px 1px 0px hsl(177, 92%, 70%)";
+
+
+        }
+
+        
+        arr.filter((item) => {
+            return item != arr[i];
+        } )
+        .forEach((item) => {
+            let hideindex = item.style.opacity = "0";
+            if (hideindex) {
+                item.style.cursor = "auto";
+            }
+        })
+        
+
+
+    })
+
+
+}
+
+
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,95 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'league-spartan' : ['"League Spartan"', 'sans-serif'],
+    },
+    colors: {
+      
+      transparent : 'transparent',
+      current : 'currentColor',
+      // Theme 1
+      // Background
+      'main-background-dark-blue': 'hsl(222, 26%, 31%)',
+      'toggle-and-keypad-background-dark-blue': 'hsl(223, 31%, 20%)',
+      'screen-background-dark-blue': 'hsl(224, 36%, 15%)',
+
+      // keys
+      'key-background-dark-blue': 'hsl(225, 21%, 49%)',
+      'key-shadow-dark-blue': 'hsl(224, 28%, 35%)',
+
+      'key-background-toggle-red': 'hsl(6, 63%, 50%)',
+      'key-shadow-dark-red': 'hsl(6, 70%, 34%)',
+
+      'key-background-light-grayish-orange': 'hsl(30, 25%, 89%)',
+      'key-shadow-grayish-orange': 'hsl(28, 16%, 65%)',
+
+      // text
+      'dark-grayish-blue': 'hsl(221, 14%, 31%)',
+      'white': 'hsl(0, 0%, 100%)',
+
+    
+      // Theme 2
+      // Backgrounds
+      'main-background-light-gray': 'hsl(0, 0%, 90%)',
+      'toggle-keypad-background-grayish-red': 'hsl(0, 5%, 81%)',
+      'screen-background-light-gray': 'hsl(0, 0%, 93%)',
+
+      // Keys
+      'key background-dark-moderate-cyan': 'hsl(185, 42%, 37%)',
+      'key-shadow-very-dark-cyan': 'hsl(185, 58%, 25%)',
+
+      'key-background-toggle-orange': 'hsl(25, 98%, 40%)',
+      'key-shadow-dark-orange': 'hsl(25, 99%, 27%)',
+
+      'key-background-light-grayish-yellow': 'hsl(45, 7%, 89%)',
+      'key-shadow-dark-grayish-orange': 'hsl(35, 11%, 61%)',
+
+      // Text
+      'dark-grayish-yellow': 'hsl(60, 10%, 19%)',
+      // 'white': 'hsl(0, 0%, 100%)'
+
+
+
+      // Theme 3
+      // Backgrounds
+      'main-background-dark-violet': 'hsl(268, 75%, 9%)',
+      'keypad-and-toggle-and-screen-background-dark-violet': 'hsl(268, 71%, 12%)',
+      
+      // #### Keys
+      'key-background-dark-violet': 'hsl(281, 89%, 26%)',
+      'key-shadow-vivid-magenta': 'hsl(285, 91%, 52%)',
+
+      'toggle-key-background-pure-cyan': 'hsl(176, 100%, 44%)',
+      'key-shadow-soft-cyan': 'hsl(177, 92%, 70%)',
+
+      'key-background-very-dark-violet': 'hsl(268, 47%, 21%)',
+      'key-shadow-dark-magenta': 'hsl(290, 70%, 36%)',
+      
+      // #### Text
+      'light-yellow': 'hsl(52, 100%, 62%)',
+      'very-dark-blue': 'hsl(198, 20%, 13%)',
+      // 'white': 'hsl(0, 0%, 100%)'
+
+    },
+    extend: {
+      
+      
+
+      screens: {
+        'phone': '320px',
+        //  => @media (min-width: 375px) { ... }
+
+        'tablet':  '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
   },
   plugins: [],
 }
